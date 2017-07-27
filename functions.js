6f6d9a22f52bf2ade2e55ce41a38152bfd80d63f
itsmedavep <itsmedavep@gmail.com>
2017-07-27 00:55:29 -0500

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,21 @@
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  $('.blue').css({
+    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+  });
+
+  $('.purple').css({
+    'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+  });
+
+  $('.yellow').css({
+    'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+  });
+  
+  $('.orange').css({
+    'transform' : 'translate(0px, -'+ wScroll /12 +'%)'
+  });
+
+});
